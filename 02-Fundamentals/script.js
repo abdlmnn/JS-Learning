20f5c8249b31a2aa54e486464f2cd7cdfb224ab0
Abdulmanan <mohammadabdulmanan24@gmail.com>
2025-06-06 23:24:42 +0800

    While loop

--- 02-Fundamentals/script.js
+++ 02-Fundamentals/script.js
@@ -493,3 +493,26 @@ for (let exercise = 1; exercise < 4; exercise++) {
   }
   console.log("\n");
 }
+
+// While loop
+console.log("---- While loop ----");
+
+// specify the condition
+let rep = 1;
+while (rep <= 10) {
+  console.log(`Lift weight repetition ${rep}`);
+  rep++;
+}
+
+console.log("\n");
+
+// Dice
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+
+  if (dice === 6) console.log("Loop is about to end...");
+}
